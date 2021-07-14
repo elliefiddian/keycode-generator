65780b790b42f89428655bd1ce0cecad208c93b4
elliefiddian <86102143+elliefiddian@users.noreply.github.com>
2021-07-14 11:51:38 +0100

    update

--- app.js
+++ app.js
@@ -5,6 +5,21 @@
 // event.which 
 // needs to be mobile friendly 
 
-const welcome = document.getElementById('welcome');
-const generator = document.getElementById('generator');
-const head = document.getElementById('keycode');
+const displaykey = document.getElementById('.key h2');
+const displayKeyCode = document.getElementById('keycode h2');
+const keycodeDiv = document.getElementById (.'keycode');
+
+
+function keycode(event) {
+var key = event.key;
+var location = event.location;
+var which = event.which;
+var code = event.code;
+head.textContent = which;
+document.getElementById("eventKey").innerHTML= key;
+document.getElementById("eventLocation").innerHTML= location;
+document.getElementById("eventWhich").innerHTML= which;
+document.getElementById("eventCode").innerHTML= code;
+generator.classList.remove("show");
+welcome.remove()
+}
