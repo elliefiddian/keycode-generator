638ca03cbd20c098a3999ce28591c8403215dc38
elliefiddian <86102143+elliefiddian@users.noreply.github.com>
2021-07-14 11:54:19 +0100

    update

--- app.js
+++ app.js
@@ -10,16 +10,3 @@ const displayKeyCode = document.getElementById('keycode h2');
 const keycodeDiv = document.getElementById (.'keycode');
 
 
-function keycode(event) {
-var key = event.key;
-var location = event.location;
-var which = event.which;
-var code = event.code;
-head.textContent = which;
-document.getElementById("eventKey").innerHTML= key;
-document.getElementById("eventLocation").innerHTML= location;
-document.getElementById("eventWhich").innerHTML= which;
-document.getElementById("eventCode").innerHTML= code;
-generator.classList.remove("show");
-welcome.remove()
-}
